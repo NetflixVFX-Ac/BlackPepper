0e544ba787c02413c93a03bb1e9f1e41e1b1478a
Jaehyuk Lee <ixero22@gmail.com>
2023-02-13 12:55:16 +0900

    fix conflicts

--- python/pepper/pepper.py
+++ python/pepper/pepper.py
@@ -9,12 +9,13 @@ class Houpub:
     _entity = None
 
     def __init__(self):
-        self.identif = None
+        self._id = None
+        pass
 
     def login(self, host, identify, password):
         gazu.client.set_host(host)
         gazu.log_in(identify, password)
-        self.identif = identify
+        # self.identif = identify
 
     @property
     def project(self):
