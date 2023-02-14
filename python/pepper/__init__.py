33779cb2e907c49e3d66cc4669e9eb1828ae2845
Sung Woo Park <veiny1990@gamil.com>
2023-02-14 15:17:05 +0900

    pepper init update psw

--- python/pepper/__init__.py
+++ python/pepper/__init__.py
@@ -1,12 +1,15 @@
 """
-What is pepper ?
-    > FX 템플릿을 각 템플릿이 캐스팅된 샷에 일괄적으로 적용한 뒤 모든 샷의 pre-render을 실행,
+*** What is pepper ? ***
+    > kitsu의 python API를 활용하여 맵핑한 API 이다.
+
+    FX 템플릿을 각 템플릿이 캐스팅된 샷에 일괄적으로 적용한 뒤 모든 샷의 pre-render을 실행,
     pre-comp 영상을 받아 아티스트가 FX의 프리비즈를 볼 수 있게 한다.\n
     Houdini 멀티샷의 기초단계로 pre-comp를 하기 위해 생성된 후디니 씬파일은 각 샷의 working file로, mov파일은 output file이 된다.
 
-How to use:
-    >
-Examples:
+*** How to use ***
+    > 1) Login
+            :
+*** Examples ***
     >
 
 
