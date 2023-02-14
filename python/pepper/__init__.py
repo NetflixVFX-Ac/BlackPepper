2ab9c6f9a95ef31e36fc6dd5db3d078ae2943c64
Sung Woo Park <veiny1990@gamil.com>
2023-02-14 14:34:00 +0900

    add pepper init

--- python/pepper/__init__.py
+++ python/pepper/__init__.py
@@ -1,1 +1,17 @@
+"""
+What is pepper ?
+    >
+How to use:
+    >
+Examples:
+    >
 
+
+*** License Copyright ***
+
+Netflix Academy 1st class Team Hook
+
+This API was created in the first season of the Netflix Academy
+and was created during the team project period by the Hook team.
+
+"""
