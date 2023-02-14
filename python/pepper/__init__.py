aa1a37fc4690fd2ce48634eada7b7cd33e9a0361
Sung Woo Park <veiny1990@gamil.com>
2023-02-14 14:42:47 +0900

    pepper init 수정중

--- python/pepper/__init__.py
+++ python/pepper/__init__.py
@@ -1,6 +1,7 @@
 """
 What is pepper ?
-    >
+    > kitsu 의 gazu를 이용하여 FX(asset)템플릿을 여러 샷에 적용 하여 pre_render 를 뽑아 아티스트가 FX가 룩을 보는데 사용되는
+    Houdini 멀티샷의 기초단계로 kitsu 의 working file path 와 output file path 를 뽑아주는 api 이다.
 How to use:
     >
 Examples:
