7c5d1bacaf630f458f70196b5824c9528fc45139
yyhoooon@gmail.com <yyhoooon@gmail.com>
2023-02-22 11:00:26 +0900

    unittest docstring update and unittest method double check

--- python/pepper/tests/test_pepper.py
+++ python/pepper/tests/test_pepper.py
@@ -9,39 +9,42 @@ class TestHoupub(TestCase):
 
 
     def setUp(self):
+        """
+        기본 로그인 값 설정
+        host : http://192.168.3.116/api
+        identify : pipeline@rapa.org
+        password : netflixacademy
+        """
         self.pepper = Houpub()
         self.pepper.login("http://192.168.3.116/api", "pipeline@rapa.org", "netflixacademy")
 
-    @property
-    def check(self):
-        return self._check
-
-    @check.setter
-    def check(self, bool):
-        self._check = bool
-
     def test_publish_working_file(self):
+        """
+        publish를 통해 working file의 last revision이 정상적으로 업데이트 됐는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
         self.pepper.entity = 'shot'
         task_type_name = 'layout'
-        software_name = 'hou'
+
         task_type, task = self.pepper.get_task(task_type_name)
         pre_revision = gazu.files.get_last_working_file_revision(task).get('revision')
-        self.pepper.publish_working_file(task_type_name, software_name)
+        self.pepper.publish_working_file(task_type_name)
         update_revision = gazu.files.get_last_working_file_revision(task).get('revision')
-        print(pre_revision, update_revision)
         self.assertLess(pre_revision, update_revision)
 
     def test_publish_output_file(self):
+        """
+        publish를 통해 output file의 last revision이 정상적으로 업데이트 됐는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
         self.pepper.entity = 'shot'
         task_type_name = 'fx'
         output_type_name = 'movie_file'
-        comments = 'for unittest_yeolhoon '
+        comments = 'for unittest_yeolhoon'
         task_type, task = self.pepper.get_task(task_type_name)
         output_type = gazu.files.get_output_type_by_name(output_type_name)
         pre_revision = gazu.files.get_last_entity_output_revision(self.pepper.entity, output_type, task_type,
@@ -53,19 +56,24 @@ class TestHoupub(TestCase):
         self.assertLess(pre_revision, update_revision)
 
     def test_working_file_path(self):
+        """
+        woking file path를 출력하여 last revisioln에 해당하는 path가 제대로 출력되는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
         self.pepper.entity = 'shot'
+        self.pepper.software = 'hip'
         task_type_name = 'layout'
-        software_name = 'hou'
-        input_num = 100
         task_type, task = self.pepper.get_task(task_type_name)
-        path = self.pepper.working_file_path(task_type_name, software_name, input_num)
+        path = self.pepper.working_file_path(task_type_name)
         latest_revision = gazu.files.get_last_working_file_revision(task).get('revision')
         self.assertEqual(latest_revision, int(path.strip()[-7:-4]))
 
     def test_make_next_working_path(self):
+        """
+        working file이 저장될 path를 출력하면, last revision에 +1이 더해진 경로로 출력되는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
@@ -77,6 +85,9 @@ class TestHoupub(TestCase):
         self.assertEqual(latest_revision + 1, int(path.strip()[-3:]))
 
     def test_output_file_path(self):
+        """
+        output file path를 출력하여 last revisioln에 해당하는 path가 제대로 출력되는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
@@ -92,6 +103,9 @@ class TestHoupub(TestCase):
         self.assertEqual(latest_revision, int(path.strip()[-7:-4]))
 
     def test_make_next_output_path(self):
+        """
+        output file이 저장될 path를 출력하면, last revision에 +1이 더해진 경로로 출력되는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
@@ -106,15 +120,23 @@ class TestHoupub(TestCase):
         self.assertEqual(latest_revision + 1, int(path.strip()[-7:-4]))
 
     def test_get_revision_num(self):
+        """
+        input_num이 revision_max보다 클 경우, revision_max로 출력되는지 체크한다.
+        input_num이 None값일 경우, revision_max로 출력되는지 체크한다.
+        """
         revision_max = 6
         input_num = 10
         mock_num = None
         check_latest = self.pepper.get_revision_num(revision_max, input_num)
         check_none = self.pepper.get_revision_num(revision_max, mock_num)
+        self.assertEqual(check_latest, check_none)
         self.assertEqual(check_latest, 6)
         self.assertEqual(check_none, 6)
 
     def test_get_task(self):
+        """
+        task_type_name에 해당하는 task_type과 task가 제대로 출력되는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0010'
@@ -124,12 +146,21 @@ class TestHoupub(TestCase):
         self.assertTrue(type(task_type), dict)
         self.assertEqual(task_type.get('name'), 'FX')
 
-    def test_get_software(self):
-        software_name = 'hou'
-        check = self.pepper.get_software(software_name)
-        self.assertEqual(check.get('file_extension'), 'hip')
+    def test_software(self):
+        """
+        software 함수에서 if문이 제대로 작동하는지 체크한다.
+        """
+        self.pepper.software = 'hipnc'
+        self.assertEqual(self.pepper.software.get('file_extension'), 'hipnc')
+        self.pepper.software = 'hip'
+        self.assertEqual(self.pepper.software.get('file_extension'), 'hip')
+        self.pepper.software = 'hiplc'
+        self.assertEqual(self.pepper.software.get('file_extension'), 'hiplc')
 
     def test_get_casting_path_for_asset(self):
+        """
+        asset에 캐스팅 된 layout의 entity가 shot의 id와 같은지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.asset = 'temp_fire'
         self.pepper.sequence = 'SQ01'
@@ -141,35 +172,75 @@ class TestHoupub(TestCase):
         #         shot_dict = last_revision[i][0]
         # self.assertIsNotNone(shot_dict)
         casted_shots, layout_tasks, fx_tasks = self.pepper.get_casting_path_for_asset()
-        pprint.pp(casted_shots)
-        pprint.pp(layout_tasks)
-        pprint.pp(fx_tasks)
+        shot = None
+        layout_task = None
+        fx_task = None
+        for i in casted_shots:
+            if i.get('shot_name') == '0010':
+                shot = i
+        self.assertEqual(shot.get('shot_name'), '0010')
+        for l in layout_tasks:
+            if l.get('entity_id') == shot.get('shot_id'):
+                layout_task = l
+        for f in fx_tasks:
+            if f.get('entity_id') == shot.get('shot_id'):
+                fx_task = f
+        self.assertEqual(layout_task.get('entity_id'), fx_task.get('entity_id'))
 
     def test_dick_check(self):
+        """
+        임의의 dictionary를 만들고 dict_check가 dictionary 형태로 반환하는지 체크한다.
+        """
         test_dict = {'hook': 'team', 'mem_num': '7'}
         code = 'none'
         check = self.pepper.dict_check(test_dict, code)
         self.assertIs(type(check), dict)
 
     def test_args_str_check(self):
+        """
+        argment가 str인지 체크한다.
+        다수의 인자가 들어올 경우, ','로 나눠서 반환하는지 체크한다.
+        *오류발생 : int값을 넣을 경우 if 문의 else로 빠지는 것이 아닌, if의 tuple일 경우로 빠진다.
+        """
+        # arg_num = 1
         arg = 'hook'
         args = 'num'
+        # check_arg_num = self.pepper.args_str_check(arg_num)
         check_arg = self.pepper.args_str_check(arg)
         check_args = self.pepper.args_str_check(arg, args)
         self.assertEqual(check_arg, arg)
         self.assertEqual(check_args, f'{arg},{args}')
 
     def test_str_check(self):
+        """
+        str을 제대로 분별하는지 체크한다.
+        int 값이 들어갔을 때, 오류를 발생하는지 체크한다.
+        """
         strn = 'hook'
+        str_num = 1
         check = self.pepper.str_check(strn)
         self.assertEqual(check, strn)
+        with self.assertRaises(Exception) as context:
+            self.pepper.str_check(str_num)
+        self.assertEqual("Input must be string", str(context.exception))
 
     def test_int_check(self):
+        """
+        int를 제대로 분별하는지 체크한다.
+        str 값이 들어갔을 때, 오류를 발생하는지 체크한다.
+        """
         num = 5
+        num_str = '5'
         check = self.pepper.int_check(num)
         self.assertEqual(check, num)
+        with self.assertRaises(Exception) as context:
+            self.pepper.int_check(num_str)
+        self.assertEqual("Input must be integer.", str(context.exception))
 
     def test_error(self):
+        """
+        code에 따라 오류가 제대로 발생하는지 체크한다.
+        """
         code = 'not_string'
         with self.assertRaises(Exception) as context:
             self.pepper.error(code)
@@ -216,6 +287,9 @@ class TestHoupub(TestCase):
         self.assertEqual("No shot or asset is assigned.", str(context.exception))
 
     def test_get_working_revision_max(self):
+        """
+        working file의 revision_max가 int값으로 제대로 출력되는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.pepper.shot = '0020'
@@ -238,6 +312,9 @@ class TestHoupub(TestCase):
         self.assertIs(type(check), int)
 
     def test_get_all_shots(self):
+        """
+        project와 sequence를 입력했을때, sequence 안에 있는 shot list를 출력하고, 안에 shot이 있는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.pepper.sequence = 'SQ01'
         self.assertIn('0010', self.pepper.get_all_shots())
@@ -247,29 +324,41 @@ class TestHoupub(TestCase):
         self.assertIn('0050', self.pepper.get_all_shots())
 
     def test_get_task_types_for_asset(self):
+        """
+        project와 asset을 입력했을때, asset안에 simulation task_type이 있는지 체크한다.
+        """
         self.pepper.project = "PEPPER"
         self.pepper.asset = "temp_explosion"
         self.assertIn("simulation", self.pepper.get_task_types_for_asset())
 
     def test_get_all_projects(self):
+        """
+        모든 프로젝트를 출력하고, 그 안에 PEPPER project가 있는지 체크한다.
+        """
         self.assertIn('PEPPER', self.pepper.get_all_projects())
 
     def test_get_all_assets(self):
+        """
+        PEPPER asset내, Template이 제대로 출력됐는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
-        self.assertIn('fx_template', self.pepper.get_all_assets())
+        self.assertIn('temp_explosion', self.pepper.get_all_assets())
+        self.assertIn('temp_fire', self.pepper.get_all_assets())
+        self.assertIn('temp_thunder', self.pepper.get_all_assets())
+        self.assertIn('temp_waterfall', self.pepper.get_all_assets())
 
     def test_get_all_sequences(self):
+        """
+        PEPPER sequence내, SQ01이 제대로 출력됐는지 체크한다.
+        """
         self.pepper.project = 'PEPPER'
         self.assertIn('SQ01', self.pepper.get_all_sequences())
 
-    def test_get_casted_assets_for_shot(self):
-        self.pepper.project = 'PEPPER'
-        self.pepper.sequence = 'SQ01'
-        self.pepper.shot = '0010'
-        self.assertIn('fx_template:temp_explosion', self.pepper.get_casted_assets_for_shot())
-
     def test_user_flow(self):
         """
+
+        user_flow에 맞춰 unittest를 진행한다.
+
         1. login unittest
         소프트웨어 assign
         self.pepper.software = 'hipnc'
@@ -404,3 +493,7 @@ class TestHoupub(TestCase):
         self.pepper.publish_precomp_working(pick_precomp)
         nwp = self.pepper.make_next_working_path('FX')
         pprint.pp(nwp)
+
+        self.pepper.publish_precomp_output(pick_precomp)
+        nwp2 = self.pepper.make_next_output_path('Movie_file', 'FX')
+        pprint.pp(nwp2)
