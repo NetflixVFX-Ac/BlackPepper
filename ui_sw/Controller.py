99f61ef4764df59baa8229cffa03ec5cba6fc7b9
Sung Woo Park <veiny1990@gamil.com>
2023-02-27 18:08:45 +0900

    end day 39

--- ui_sw/Controller.py
+++ ui_sw/Controller.py
@@ -7,22 +7,28 @@ View : "login.ui, main.ui"
 """
 
 """
-email:
+
 pepper@hook.com
-pw :
 pepperpepper
+
+pipeline@rapa.org
+netflixacademy
 """
 
 
 
 import sys
+
+
 from PySide2.QtUiTools import QUiLoader
 from PySide2 import QtWidgets, QtCore
+from PySide2.QtGui import QStandardItem, QStandardItemModel
+
 
 import Model
 
 
-class PepC(QtWidgets.QMainWindow):
+class PepC(QtWidgets.QListView):
 
     def __init__(self, loginUi_file, mainUi_file):
         super(PepC, self).__init__()
@@ -35,33 +41,71 @@ class PepC(QtWidgets.QMainWindow):
         loader = QUiLoader()
         self.login_ui = loader.load(loginUi)
         self.main_ui = loader.load(mainui_file)
-        loginUi.close()
+        # loginUi.close()
 
-        # setUI
-        # login UI
+        # set login UI
         self.login_ui.login_btn.clicked.connect(self.set_login)
         self.login_ui.setWindowTitle("Pepper Login v0.0.1 SW")
         self.login_ui.show()
 
-        # main UI
-
+        # set main UI
         self.main_ui.setWindowTitle("Pepper Main v0.0.1 SW")
         self.main_ui.show()
 
+        self.main_ui.project_list.itemClicked.connect(self.set_temp)
+        # self.main_ui.project_list.currentItem()
     def set_login(self):
-
+        # get login data
         email = self.login_ui.input_id.text()
         pw = self.login_ui.input_pw.text()
         software_ext = self.login_ui.hipbox.currentText()[1:]
         pepm = Model.PepM()
-        pepm.login_model(email, pw, sel_software=software_ext)
+        pepm.login_data(email, pw, sel_software=software_ext)
 
-        self.set_main()
+        # login 하면 로그인창 close 되고 로그인정보에 어사인된 프로젝트리스트가져오는 함수 까지 작동
+        self.login_ui.close()
+        self.set_project()
 
-
-    def set_main(self):
+    def set_project(self):
         pepm = Model.PepM()
-        proj_list = pepm.main_model()
+        project_list = pepm.get_project()
+        model = QStandarditemModel()
+
+        for item in project_list:
+            model.appendRow(QStandardItem(item))
+        self.main_ui.list.listView_proj.setModel(model)
+
+
+
+        # self.main_ui.project_list.clear()
+        # self.main_ui.project_list.addItems(proj_list)
+
+        # temp_list = pepm.get_temp(project_name)
+        # self.main_ui.temp_list.clear()
+        # self.main_ui.temp_list.addItems(temp_list)
+        # self.main_ui.show()
+
+    def set_clear(self):
+        pass
+
+
+
+    def set_temp(self, projects=None):
+        pepm = Model.PepM()
+        project_name = projects[self.main_ui.project_list.currentRow()]
+        print(project_name)
+        # temps = pepm.get_temp(project_name)
+        # print(temps)
+
+
+
+    #
+    #     self.main_ui.temp_list.clear()
+    #     self.main_ui.temp_list.addItems(temp_list)
+    #     # self.main_ui.show()
+
+    def set_shot(self):
+        pass
 
 
 def main():
--- ui_sw/Model.py
+++ ui_sw/Model.py
@@ -10,22 +10,48 @@ class PepM:
 
     def __init__(self):
         self.host = "http://192.168.3.116/api"
-        self.email = None
-        self.pw = None
-        self.sel_software = None
-        self.project_list = None
+        self.email = "pepper@hook.com"
+        self.pw = "pepperpepper"
+        self.sel_software = "hip"
 
-    def login_model(self, email, pw, sel_software):
+    def login_data(self, email, pw, sel_software):
 
         self.email = email
         self.pw = pw
-        pepper = Houpub()
-        pepper.login(self.host, email, pw)
-        pepper.software = sel_software
+        self.sel_software = sel_software
+        pep = Houpub()
+        pep.login(self.host, email, pw)
+        pep.software = sel_software
         print(f"login id : {email} , seleted software : {sel_software}")
 
-    def main_model(self):
-        pepper = Houpub()
-        get_myproject = pepper.get_my_projects()
-        print(f"get user project : {get_myproject}")
+    def get_project(self):
+        pep = Houpub()
+        pep.get_my_projects()
 
+    def get_temp(self, project_name):
+        pep = Houpub()
+        pep.project = project_name
+        # print(pep.project)
+        asset_list = pep.get_all_assets()
+        # print(asset_list)
+        return asset_list
+
+
+
+
+
+# def main():
+#     c = PepM()
+#     # c.email = "pepper@hook.com"
+#     # c.pw = "pepperpepper"
+#     c.email = "pipeline@rapa.org"
+#     c.pw = "netflixacademy"
+#     c.sel_software = "hip"
+#     c.login_data(c.email, c.pw, c.sel_software)
+#     project_name = c.get_project()
+#     c.get_temp(project_name[0])
+#     # pass
+#
+#
+# if __name__ == "__main__":
+#     main()
