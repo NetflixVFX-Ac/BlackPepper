a1b4aa589524f277a2213383736569ce8a3c3ab1
Sung Woo Park <veiny1990@gamil.com>
2023-03-02 13:14:13 +0900

    edit

--- ui_sw/window.py
+++ ui_sw/window.py
@@ -50,7 +50,7 @@ class MainWindow:
         self.model_render = MainModel()
 
         # login Ui loader
-        login_ui_file = "/ui_sw/login.ui"
+        login_ui_file = "/home/rapa/login.ui"
         login_ui = QtCore.QFile(login_ui_file)
         login_ui.open(QtCore.QFile.ReadOnly)
         loader = QUiLoader()
@@ -58,7 +58,7 @@ class MainWindow:
         self.window_login.show()
 
         # main Ui loader
-        main_ui_file = "/ui_sw/main.ui"
+        main_ui_file = "/home/rapa/main.ui"
         main_ui = QtCore.QFile(main_ui_file)
         main_ui.open(QtCore.QFile.ReadOnly)
         loader = QUiLoader()
