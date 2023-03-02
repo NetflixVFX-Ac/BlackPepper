b579bd67a16f3686fa9cfebd355b393932049db3
yyhoooon@gmail.com <yyhoooon@gmail.com>
2023-03-02 15:05:07 +0900

    test edit

---
+++ python/pepper/tests/test_houpepper.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+
+class TestHouPepper(TestCase):
+    def test_abc_path(self):
+        self.fail()
+
+    def test_abc_path(self):
+        self.fail()
+
+    def test_abc_tree_all(self):
+        self.fail()
+
+    def test_abc_tree_all(self):
+        self.fail()
+
+    def test_abc_tree_path(self):
+        self.fail()
+
+    def test_abc_tree_path(self):
+        self.fail()
+
+    def test_abc_range(self):
+        self.fail()
+
+    def test_abc_range(self):
+        self.fail()
+
+    def test_set_abc_cam_tree(self):
+        self.fail()
+
+    def test_get_abc_cam_tree(self):
+        self.fail()
+
+    def test_check_abc(self):
+        self.fail()
+
+    def test_set_cam_view(self):
+        self.fail()
+
+    def test_get_cam_resolution(self):
+        self.fail()
+
+    def test_get_cam_xform(self):
+        self.fail()
+
+    def test_set_cam_key(self):
+        self.fail()
+
+    def test_set_cam_create(self):
+        self.fail()
+
+    def test_set_fx_working_for_shot(self):
+        self.fail()
+
+    def test_set_mantra_for_render(self):
+        self.fail()
+
+    def test_set_ffmpeg_seq_to_mov(self):
+        self.fail()
