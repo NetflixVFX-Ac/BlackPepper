8ebb9ad1e9a75be65f6fbe992bf8bd2d6695e168
jinkwang <Qkrlwsrhkd11@gmail.com>
2023-03-15 16:12:57 +0900

    thum_update

--- python/BlackPepper/ui/thum_updater.py
+++ python/BlackPepper/ui/thum_updater.py
@@ -5,6 +5,8 @@ import os
 import glob
 import subprocess
 import gazu
+import random
+
 
 class thumnail_pusher():
 
@@ -17,7 +19,6 @@ class thumnail_pusher():
                     file_path = os.path.join(root, file)
                     file_path = '"' + file_path + '"'
                     print(file_path)
-
 
     def thumnail_machine(self):
         input = "/home/rapa/dog_convert.mov"
@@ -37,11 +38,11 @@ class thumnail_pusher():
         cmd = (' '.join(str(s) for s in command))
         result = subprocess.run(cmd, shell=True, check=True)
 
-
-    def upload_thumnail_to_kitsu(self):
+    def upload_thumnail(self):
         # Gazu 서버에 로그인합니다.
         # gazu.login("http://192.168.3.116/login", "pipeline@rapa.org", "netflixacademy")
-        self.set_auto_login()
+        gazu.set_host("http://192.168.3.116/api")
+        gazu.log_in("pipeline@rapa.org", "netflixacademy")
 
         project_name = "hoon"
 
@@ -51,52 +52,47 @@ class thumnail_pusher():
 
         image_path = "/home/rapa/thumbnail.jpg"
 
-        project = gazu.project.get_by_name(project_name)
+        project = gazu.project.get_project_by_name(project_name)
+        print("1111", project["name"], project["id"])
 
-        asset = gazu.asset.get_by_name(project["id"], asset_name)
-        print(asset)
+        asset = gazu.asset.get_asset_by_name(project["id"], asset_name)
+        print("2222", asset["name"], asset["id"])
 
-        shot = next((s for s in gazu.asset.get_shots(asset["id"]) if s["name"] == shot_name), None)
+        shot = next((s for s in gazu.asset.all_shot(asset["id"]) if s["name"] == shot_name), None)
+        print("3333", shot)
 
         if shot is None:
             print(f"No shot named {shot_name} found for asset {asset_name}")
             exit(1)
 
-        image = gazu.files.upload(image_path)
+        image = gazu.files.update_preview(image_path)
 
         gazu.shots.attach_image(shot["id"], image["id"])
 
+    def find_jpg(self):
+
+        folder_path = "/home/rapa/"
+
+        # 폴더 내에 있는 파일 중 JPG 파일만 리스트로 가져옵니다.
+        file_list = [f for f in os.listdir(folder_path) if f.endswith(".jpg")]
+
+        if len(file_list) == 0:
+            print(f"No JPG files found in {folder_path}")
+            exit(1)
+
+        # 파일 리스트에서 랜덤으로 선택합니다.
+        random_file = random.choice(file_list)
+
+        # 선택된 파일의 전체 경로를 출력합니다.
+        random_file_path = os.path.join(folder_path, random_file)
+        print(random_file_path)
 
 
-    def set_auto_login(self):
-        log_path = self.login_log.user_path
-        self.login_log.host = "http://192.168.3.116/api"
-        log_id = self.login_window.input_id.text()
-        log_pw = self.login_window.input_pw.text()
-        log_sfw = self.login_window.hipbox.currentText()[1:]
-        log_value = self.login_log.load_setting()
-        log_dict = self.login_log.user_dict
-        if os.path.exists(log_path) and (not log_dict['auto'] or log_id != log_value['user_id']
-                                         or log_pw != log_value['user_pw'] or log_sfw != log_value['user_ext']):
-            self.login_log.user_id = log_id
-            self.login_log.user_pw = log_pw
-            self.login_log.user_ext = log_sfw
-            self.login_log.valid_host = True
-            self.login_log.valid_user = True
-            self.login_log.auto_login = True
-            self.login_log.save_setting()
-            return
-        if log_value['valid_host'] and log_value['valid_user']:
-            self.login_log.host = log_value['host']
-            self.login_log.user_id = log_value['user_id']
-            self.login_log.user_pw = log_value['user_pw']
-            self.login_log.user_ext = log_value['user_ext']
-            self.pepper.login(self.login_log.host, self.login_log.user_id, self.login_log.user_pw)
-            self.pepper.software = self.login_log.user_ext
-            self.login_window.close()
-            self.open_main_window()
-        else:
-            pass
+def main():
+    m = thumnail_pusher()
+    m.find_jpg()
+    m.upload_thumnail()
 
 
-thumnail_pusher.upload_thumnail_to_kitsu()
+if __name__ == "__main__":
+    main()
