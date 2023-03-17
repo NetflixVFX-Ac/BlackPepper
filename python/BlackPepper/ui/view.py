191ec80c5a7ae4f147efddb463e5a22ecad20438
Jaehyuk Lee <ixero22@gmail.com>
2023-03-17 15:03:42 +0900

    add docstrings to view.py and model.py

--- python/BlackPepper/ui/view.py
+++ python/BlackPepper/ui/view.py
@@ -2,21 +2,17 @@ from PySide2 import QtWidgets
 
 
 class PepperView(QtWidgets.QListView):
-    """
-
+    """QtWidget의 QListView를 상속받 Viewer class로, project_listview, templates_listview, shots_listview에 사용된다.
     """
     def __init__(self, parent):
         super(PepperView, self).__init__(parent=None)
-
-    def get_selected_data(self):
-        if not self.model():
-            return
-        return self.model().selectedIndexes()[-1]
+        pass
 
 
 class PepperDnDView(QtWidgets.QListView):
-    """
-
+    """QtWidget의 QListView를 상속받는 Viewer class로, drag & drop event를 accept해주는 메소드들을 가지고 있다.
+    Drag & drop은 renderlists_listview에만 사용되며,
+    Drag
     """
     def __init__(self, parent):
         super(PepperDnDView, self).__init__(parent=None)
@@ -26,15 +22,18 @@ class PepperDnDView(QtWidgets.QListView):
         self.startIndex = None
 
     def dragEnterEvent(self, event):
-        """
-
+        """Listview 안에서 하나의 row를 drag 했을 때 생기는 이벤트를 accept 해주는 메소드다.
         """
         self.startIndex = ''
         event.accept()
 
     def dropEvent(self, event):
-        """
-
+        """Listview 안에서 drag된 row를 drop 했을 때 생기는 이벤트를 accept 해주는 메소드다. \n
+        선택된 row의 index를 selections로 받고, drop된 row의 index는 destination_index로 받는다.
+        기존 model.pepperlist의 data들은 updated_list에 저장해두고,
+        destination_index에 맞는 위치에 selections의 data를 insert 한다.
+        insert 이후에는 updated_list에서 기존 selections의 row를 pop을 통해 제거해준다. \n
+        업데이트된 updated_list는 다시 model.pepperlist의 data로 들어간다.
         """
         selections = self.selectedIndexes()
         rev_selections = list(reversed(selections))
